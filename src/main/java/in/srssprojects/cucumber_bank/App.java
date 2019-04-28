5290034482637587f8328d4d3b138abc33e4a936
automation <atcsurya@gmail.com>
2019-04-28 09:04:00 +0530

    initial upload

---
+++ src/main/java/in/srssprojects/cucumber_bank/App.java
@@ -0,0 +1,13 @@
+package in.srssprojects.cucumber_bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
